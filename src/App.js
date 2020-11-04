dd627a6696999a84a3d0461a497e6e5e050c2a40
Lư Trung <lutrung1297@gmail.com>
2020-11-04 22:48:41 +0700

    'done'

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BaiTapMatKinh from './BTLayoutMatKinh';
-import BaiTapOanTuXi from './BTOanTuXi';
 
 function App() {
   return (
     <div className="App">
-        {/* <BaiTapMatKinh/> */}
-        <BaiTapOanTuXi/>
+        <BaiTapMatKinh/>
     </div>
   );
 }
